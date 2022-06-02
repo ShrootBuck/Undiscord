b1d273cc313c4cec13d0f7cb14b24b5dd8bd8405
ShrootBuck <zaydkrunz@gmail.com>
2022-06-02 20:41:09 +0200

    More travelling = more syncing between devices

--- Undiscord.py
+++ Undiscord.py
@@ -4,52 +4,29 @@
 
 # Imports
 
-from time import sleep
+import time
+from zipfile import ZipFile
 from tkinter import filedialog
 import tkinter as Tkinter
 import os
 
 
-# Variables
-
-ConsoleColors = {
-    "Green":		"\033[92m",
-    "Yellow":		"\033[93m",
-    "Blue":		"\033[94m",
-    "Pink":		"\033[95m",
-    "Cyan":		"\033[96m",
-    "Red":		"\033[91m",
-    "Bold":		"\033[1m",
-    "Underline":	"\033[4m"
-}
-
-
 # Functions
 
-def Debug(Message,  Prefix="DEBUG", ColorMessage=False, IsInput=False, *Colors):
 
-    # Add all styling
-    for Value in Colors:
-        FormattedMessage = ConsoleColors[Value]
-
-    # Add the actual message and color terminator
-    if (ColorMessage):
-        FormattedMessage = FormattedMessage + \
-            "[" + Prefix + "]:\t" + Message + "\033[0m"
-    else:
-        FormattedMessage = FormattedMessage + \
-            "[" + Prefix + "]:\t" + "\033[0m" + Message
+def Debug(Message, Prefix="DEBUG", IsInput=False):
+    FormattedMessage = "[" + Prefix + "]:\t" + Message
 
     # Behavior determined on whether it's an input or not
-    if (IsInput):
+    if IsInput:
         return input(FormattedMessage)
     else:
         print(FormattedMessage)
 
 
 def HangProcess():
-    while (True):
-        sleep(1000)
+    while True:
+        time.sleep(1000)
 
 
 def ClearConsole():
@@ -60,26 +37,61 @@ def ClearConsole():
     os.system(Command)
 
 
-def PromptFileUpload():
+def PromptFileUpload(
+    AllowedFiles, Title="Please select a file.", InitialDirectory=os.getcwd()
+):
     Root = Tkinter.Tk()
     Root.withdraw()  # We don't actually need a UI from TK
-    return filedialog.askopenfilename()
+    return filedialog.askopenfilename(
+        initialdir=InitialDirectory, title=Title, filetypes=AllowedFiles
+    )
+
+
+def ExtractZip(FileName, Destination=None, Password=None):
+    with ZipFile(FileName, "r") as ZipObject:
+        ZipObject.extractall(Destination, None, Password)
+
+
+# Get the Auth Token
+AuthorizationToken = Debug("Enter your Discord authorization token:", "OPTIONS", True)
 
 
 # Attempt to convert it to an integer-represented value
 UserSelection = None
 IntParseSuccess = False  # I know, bad way of handling this, don't talk to me about it
 
-while (IntParseSuccess == False):
+while IntParseSuccess == False:
     UserSelection = Debug(
-        "From where should Undiscord delete messages?\n\t\t[1]:\tAll\n\t\t[2]:\tServers\n\t\t[3]:\tDMs\n\t\t[4]:\tNone\n:", "OPTIONS", False, True, "Cyan")
+        "From where should Undiscord delete messages?\n\t\t[1]:\tAll\n\t\t[2]:\tServers\n\t\t[3]:\tDMs\n\t\t[4]:\tNone\n:",
+        "OPTIONS",
+        True,
+    )
     # Try parsing
     try:
         UserSelection = int(UserSelection)
+        IntParseSuccess = True
     except ValueError:
         ClearConsole()
-        Debug("Please select a valid option!", "ERROR", True, False, "Red")
+        Debug(
+            "Please select a valid option!",
+            "ERROR",
+            False,
+        )
 
+
+# Unzip the data package
+Debug("Please import your Discord data package.", "OPTIONS", False)
+time.sleep(1)  # Funky
+ArchivePath = PromptFileUpload(
+    [("ZIP Files", "*.zip")], "Please import your Discord data package."
+)
+
+try:
+    ExtractZip(ArchivePath, "temp")
+except Exception as e:
+    print(e)
+
+time.sleep(10)
 match UserSelection:
     case 2:
         print("e")
