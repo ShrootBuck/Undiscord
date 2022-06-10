d0ce02c9e9c56f92824ce42329f92f3c26905210
ShrootBuck <zaydkrunz@gmail.com>
2022-06-10 23:25:03 +0200

    Somewhat works now

--- README.md
+++ README.md
@@ -3,6 +3,3 @@ I think it's pretty obvious, but this is not a released tool yet.
 
 # Undiscord
 A tool to delete all of your Discord messages.
-
-# Like the tool?
-[!["Buy Me A Coffee"](https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png)](https://www.buymeacoffee.com/shrootbuck)
--- Undiscord.py
+++ Undiscord.py
@@ -5,6 +5,7 @@
 # Imports
 
 import json
+import requests
 import time
 from zipfile import ZipFile
 from tkinter import filedialog
@@ -55,7 +56,16 @@ def ReadFromZip(ZipName, FileName):
 
 
 # Get the Auth Token
-AuthorizationToken = Debug("Enter your Discord authorization token:\t", "OPTIONS", True)
+AuthorizationToken = Debug("Enter your Discord authorization token: ", "OPTIONS", True)
+
+
+# Get the User ID
+UserID = Debug("Enter your Discord user ID: ", "OPTIONS", True)
+
+
+# Session
+MainSession = requests.session()
+MainSession.headers = {"authorization": AuthorizationToken}
 
 
 # Attempt to convert it to an integer-represented value
@@ -64,7 +74,7 @@ IntParseSuccess = False  # I know, bad way of handling this, don't talk to me ab
 
 while IntParseSuccess == False:
     UserSelection = Debug(
-        "From where should Undiscord delete messages?\n\t\t[1]:\tAll\n\t\t[2]:\tServers\n\t\t[3]:\tDMs\n\t\t[4]:\tNone\n:\t",
+        "From where should Undiscord delete messages?\n\t\t[1]:\tServers\n\t\t[2]:\tDMs\n:\t",
         "OPTIONS",
         True,
     )
@@ -73,7 +83,6 @@ while IntParseSuccess == False:
         UserSelection = int(UserSelection)
         IntParseSuccess = True
     except ValueError:
-        ClearConsole()
         Debug(
             "Please select a valid option!",
             "ERROR",
@@ -85,6 +94,7 @@ while IntParseSuccess == False:
 
 ZipReadFailure = True
 MessageIndexJSON = "I do a little trolling."
+CurrentServerListJSON = "I do a little trolling."  # Used later
 
 # Ensure auto-retry
 while ZipReadFailure:
@@ -94,29 +104,107 @@ while ZipReadFailure:
             [("ZIP Files", "*.zip")], "Please import your Discord data package."
         )
         MessageIndexJSON = ReadFromZip(ArchivePath, "messages/index.json")
+        CurrentServerListJSON = ReadFromZip(ArchivePath, "servers/index.json")
+
         ZipReadFailure = False
+
+        ClearConsole()
 
     except Exception as ReadZIPException:
         Debug(ReadZIPException, "ERROR", False)
         ZipReadFailure = True  # Shouldn't be necessary, but good practice
 
 MessageIndex = json.loads(MessageIndexJSON)
+CurrentServerList = json.loads(CurrentServerListJSON)
 
-# Get the channels to purge
+# Get the channels to index
 Channels = {"DM": [], "Server": []}
+
+ChannelsToPurge = []
 
 # DMs
 for Index in MessageIndex:
     if MessageIndex[Index].startswith("Direct Message with"):
         Channels["DM"].append(Index)
-    else:
-        # Server
-        Channels["Server"].append(Index)
 
 
-# DM fetch: https://discord.com/api/v9/channels/ChannelID/messages?limit=100
+# Servers
+for Index in CurrentServerList.keys():
+    Channels["Server"].append(Index)
+
+
+# Easy way to break nested loops
+class BreakNestedLoop(Exception):
+    pass
+
+
+def QueryChannelMessages(ID, IsServer=True):
+    Messages = []
+    Data = {"Offset": 0}
+
+    # Begin querying
+    try:
+        while "WOAH" == "WOAH":
+            if IsServer == True:
+                QueryURL = f"https://discord.com/api/v9/guilds/{ID}/messages/search?author_id={UserID}&offset={str(Data['Offset'])}"
+            else:
+                QueryURL = f"https://discord.com/api/v9/channels/{ID}/messages/search?author_id={UserID}&offset={str(Data['Offset'])}"
+            Query = MainSession.get(QueryURL)
+            match Query.status_code:
+                case 200:
+                    # Save messages
+                    for Message in Query.json()["messages"]:
+                        Messages.append(Message[0])
+
+                    # If we're done
+                    if len(Query.json()["messages"]) < 25:
+                        raise BreakNestedLoop
+                    else:
+                        Data["Offset"] += 25
+                case 202:  # Channel needs to index
+                    time.sleep(2)
+                case 403:  # No access to channel
+                    raise BreakNestedLoop
+                case 429:  # Ratelimit
+                    if "retry_after" in Query.json().keys():
+                        time.sleep(int(Query.json()["retry_after"]))
+    except BreakNestedLoop:
+        pass
+    return Messages
+
+
+AmountDeleted = 0
 
 
 match UserSelection:
-    case 2:
-        print("e")
+    case 1:  # Servers
+
+        for Server in Channels["Server"]:
+            ServerMessages = QueryChannelMessages(Server, True)
+
+            for Message in ServerMessages:
+                try:
+                    while True:
+                        DeleteRequest = MainSession.delete(
+                            f"https://discord.com/api/v9/channels/{Message['channel_id']}/messages/{Message['id']}"
+                        )
+
+                        match DeleteRequest.status_code:
+                            case 204:  # Success
+                                AmountDeleted += 1
+                                raise BreakNestedLoop
+                            case 403:  # No access anymore
+                                Debug(
+                                    f"No access to channel {Message['channel_id']} in server {Server}"
+                                )
+                                raise BreakNestedLoop
+                            case 429:  # Ratelimit
+                                if "retry_after" in DeleteRequest.json().keys():
+                                    time.sleep(int(DeleteRequest.json()["retry_after"]))
+                except BreakNestedLoop:
+                    pass
+
+            Debug(f"Finished clearing messages in server {Server}")
+
+ClearConsole()
+Debug(f"Deleted {AmountDeleted} messages!", "DEBUG", True)
